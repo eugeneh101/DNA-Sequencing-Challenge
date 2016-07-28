dba8c6f3b73634c99ac6cba92c78bcc338ba25cc
eugeneh101 <eugeneh101@yahoo.com>
2016-07-28 16:48:43 -0700

    descriptions added

---
+++ README.md
@@ -0,0 +1,12 @@
+Hello! My name is Eugene, and I've been hard at work at this coding challenge.  
+
+Here are the files and what they do. I hope you enjoyed my solution:  
+PROBLEM_STATEMENT.md: The question I received  
+coding_challenge_data_set.txt: The data set I received
+coding_challenge.py: The actual code file  
+Code-Review.ipynb: The same code in iPython Notebook form  
+Coding_Notes_and_Runtime_Analysis.md: The file that explains more in detail about optimizations and tradeoffs that code makes  
+unit_test.py: I created 6 test cases to test whether my code is robust against edge cases.  
+test_cases: a folder with 6 different test data I made up
+
+I hope you enjoyed looking through my code. I look forward to meeting you in person!  
--- unit_tests.py
+++ unit_tests.py
@@ -5,29 +5,33 @@ import coding_challenge
 class TestGeneSequencer(unittest.TestCase):
 
 	def test1(self):
-		self.assertEqual(coding_challenge.main('sample_example.txt'), 
+		self.assertEqual(coding_challenge.main(
+			'test_cases/sample_example.txt'), 
 			'ATTAGACCTGCCGGAATAC')
 
 	def test2(self):
-		self.assertEqual(coding_challenge.main('sample_example_shuffled.txt'), 
+		self.assertEqual(coding_challenge.main(
+			'test_cases/sample_example_shuffled.txt'), 
 			'ATTAGACCTGCCGGAATAC')
 
 	def test3(self):
-		self.assertEqual(coding_challenge.main('cannot_detect_left.txt'), 
+		self.assertEqual(coding_challenge.main(
+			'test_cases/cannot_detect_left.txt'), 
 			'AAAAACTGATAGATTTAAAAAG')
 
 	def test4(self):
-		self.assertEqual(coding_challenge.main('cannot_detect_right.txt'), 
+		self.assertEqual(coding_challenge.main(
+			'test_cases/cannot_detect_right.txt'), 
 			'ATTAGGAAAAAGTCGAAAAA')
 
 	def test5(self):
 		self.assertEqual(coding_challenge.main(
-			'cannot_detect_left_or_right.txt'), 
+			'test_cases/cannot_detect_left_or_right.txt'), 
 			'AAAAACTGATAGATTTAAAAAGATTAGGTTTTTGTCGTTTTT')
 
 	def test6(self):
 		self.assertEqual(coding_challenge.main(
-			'cannot_detect_left_or_right_messy.txt'), 
+			'test_cases/cannot_detect_left_or_right_messy.txt'), 
 			'AAAAACTGATAGATTTAAAAAGATTAGGTTTTTGTCGTTTTT')
 
 
